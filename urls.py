d6c8f6591c860fc45eea3e8e984e556bbcb94ebd
uday9866 <35257568+uday9866@users.noreply.github.com>
2020-04-08 08:55:06 +0400

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,9 @@
+from django.urls import re_path
+from . import views
+app_name = 'accounts'
+
+urlpatterns = [
+    re_path(r'^signup/$', views.signup_view,name = "signup"),
+    re_path(r'^login/$',views.login_view,name="login"),
+    re_path(r'^logout/$',views.logout_view,name="logout"),
+]
